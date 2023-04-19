7572575ebbe53c84eb41e0dc41f1ce865391dcd9
Karen Karslyan <k.karslian@mail.ru>
2023-04-19 18:22:21 +0300

    Click succes

--- src/test/java/ScooFaqTest/Chrome/Chtest.java
+++ src/test/java/ScooFaqTest/Chrome/Chtest.java
@@ -12,15 +12,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.By.*;
+
 
 @RunWith(Parameterized.class)
 public class Chtest {
@@ -28,13 +25,13 @@ public class Chtest {
 
     WebDriver driver;
 
-    private final String index;
+    private final int index;
     private final int panelNumber;
     private final String panelText;
     private final boolean result;
 
 
-    public Chtest(String index, int panelNumber, String panelText, boolean result) {
+    public Chtest(int index, int panelNumber, String panelText, boolean result) {
         this.index = index;
         this.panelNumber = panelNumber;
         this.panelText = panelText;
@@ -45,14 +42,14 @@ public class Chtest {
     public static Object[][] openFaqAnswer() {
         return new Object[][]{
 
-                {".//div[@class = 'accordion__item'][1]", 0,"Сутки — 400 рублей. Оплата курьеру — наличными или картой.", true},
-                {".//div[@class = 'accordion__item'][2]", 1,"Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.", true},
-                {".//div[@class = 'accordion__item'][3]", 2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.", true},
-                {".//div[@class = 'accordion__item'][4]", 3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее.", true},
-                {".//div[@class = 'accordion__item'][5]", 4, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.", true},
-                {".//div[@class = 'accordion__item'][6]", 5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.", true},
-                {".//div[@class = 'accordion__item'][7]", 6, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.", true},
-                {".//div[@class = 'accordion__item'][8]", 7, "Да, обязательно. Всем самокатов! И Москве, и Московской области.", true},
+                {0, 0,"Сутки — 400 рублей. Оплата курьеру — наличными или картой.", true},
+                {1, 1,"Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.", true},
+                {2, 2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.", true},
+                {3, 3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее.", true},
+                {4, 4, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.", true},
+                {5, 5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.", true},
+                {6, 6, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.", true},
+                {7, 7, "Да, обязательно. Всем самокатов! И Москве, и Московской области.", true},
         };
     }
 
@@ -73,17 +70,15 @@ public class Chtest {
     @Test
     public void mainPage_fAQ_checkText() {
 
+        List<WebElement> elements = driver.findElements(By.xpath(".//div[@class = 'accordion__item']"));
+        WebElement elementsItem = elements.get(index);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",
+                elementsItem);
+        new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.elementToBeClickable(elementsItem));
+        elementsItem.click();
 
 
-
-            WebElement heading = xpath(index).findElement(driver);
-
-            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",
-                    heading);
-            new WebDriverWait(driver, 10)
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath(index).findElement(driver)));
-
-            heading.click();
 
         var panelId =  "accordion__panel-" + panelNumber;
         WebElement panel = id(panelId).findElement(driver);
